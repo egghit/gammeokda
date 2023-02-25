2efade7a1f1be672a73485862fdbb6ee4db1fae9
se030 <se030.kim@gmail.com>
2023-02-25 14:03:40 +0900

    [settings]: vite config에 tsconfigPaths 플러그인 추가

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { resolve } from 'path';
 
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   resolve: {
     alias: [{ find: '@', replacement: `${resolve()}/src/components` }],
   },
