dcee1abb6ec1c7324ec468249f175b2425fb08f8
star <chlvkfksquf@gmail.com>
2023-02-25 17:23:03 +0900

    [modify] file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+
+import CalendarPage from '@/pages/CalendarPage';
+import IndexPage from '@/pages/IndexPage';
+import GlobalStyle from '@/styles/reset';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <IndexPage />,
+  },
+  { path: 'calendar', element: <CalendarPage /> },
+]);
+
 const App = () => {
-  return <div />;
+  return (
+    <>
+      <GlobalStyle />
+      <RecoilRoot>
+        <RouterProvider router={router} />
+      </RecoilRoot>
+    </>
+  );
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { RecoilRoot } from 'recoil';
 
 import App from './App';
-import Calendar from './components/CalendarPage/Calendar';
 
-import GlobalStyle from '@/styles/reset';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-  { path: 'calendar', element: <Calendar /> },
-]);
-
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <RecoilRoot>
-    <GlobalStyle />
-    <RouterProvider router={router} />
-  </RecoilRoot>,
-);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />);
