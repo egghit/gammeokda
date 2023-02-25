380a189929efe13612ee04faaaeb64006147ad4a
star <chlvkfksquf@gmail.com>
2023-02-25 17:09:39 +0900

    [fix] order

--- src/main.tsx
+++ src/main.tsx
@@ -17,7 +17,7 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <RecoilRoot>
-    <RouterProvider router={router} />
     <GlobalStyle />
+    <RouterProvider router={router} />
   </RecoilRoot>,
 );
