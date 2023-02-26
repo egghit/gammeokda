b763d28a5eb255a60b882922fd8102626c1e36fe
star <chlvkfksquf@gmail.com>
2023-02-27 00:30:33 +0900

    [add] utils emotion

---
+++ src/utils/emotion.ts
@@ -0,0 +1,9 @@
+import emotionIcon from '@/assets/emotionIcon';
+
+export const filterEmotion = (emotion: string) => {
+  if (emotion === '행복') return emotionIcon.happyActive;
+  if (emotion === '슬픔') return emotionIcon.sadActive;
+  if (emotion === '기쁨') return emotionIcon.delightActive;
+  if (emotion === '보통') return emotionIcon.sosoActive;
+  if (emotion === '화남') return emotionIcon.angryActive;
+};
