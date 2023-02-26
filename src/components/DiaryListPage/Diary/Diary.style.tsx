625552a1aac7a04c8727d89ef3d32192c3ceca72
sbshin <sbshin.dev@gmail.com>
2023-02-26 10:47:24 +0900

    Feat/diary layout (#5)
    * [feat] 다이어리 컴포넌트 레이아웃 구성
    
    * [feat] 컴포넌트 배치, 더미데이터로 테스트
    
    * [fix] 빌드시 오류나는 부분 수정

---
+++ src/components/DiaryListPage/Diary/Diary.style.tsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const ItemWrapper = styled.article`
+  margin-bottom: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const DiaryDate = styled.p``;
+export const Info = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ActionButton = styled.button``;
+
+export const ScrollToTopButton = styled.button``;
