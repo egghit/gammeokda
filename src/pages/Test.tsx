c8a81f94b70c6e3ad5a20b77e147469d5b4252e8
sodalite1204 <91005609+sodalite1204@users.noreply.github.com>
2023-02-26 23:29:12 +0900

    [feat] diary style (#16)
    * [feat] Add folder contents
    
    * [feat] Add diary style
    
    * [refactor] Modify constant naming and style

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,7 +1,7 @@
 import { collection, getDocs, doc, setDoc, Timestamp } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
 
-import { Diary } from '@/@types/types';
+import { DiaryContent } from '@/@types/types';
 import Modal from '@/components/common/Modal';
 import { db } from '@/firebase';
 
@@ -22,7 +22,7 @@ const Test = () => {
     }
   };
 
-  const postDiaryTestData = async (postData: Diary) => {
+  const postDiaryTestData = async (postData: DiaryContent) => {
     try {
       //
       await setDoc(doc(db, 'diary', 'test'), postData);
@@ -32,7 +32,7 @@ const Test = () => {
     }
   };
 
-  const diaryData: Diary = {
+  const diaryData: DiaryContent = {
     date: Timestamp.fromDate(new Date('December 10, 2022')),
     emotion: '기쁨',
     photo: 'http://',
--- src/styles/reset.ts
+++ src/styles/reset.ts
@@ -1,5 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
+import background from '@/assets/background.png';
+
 const GlobalStyles = createGlobalStyle`
   html, body, div, span, applet, object, iframe,
   h1, h2, h3, h4, h5, h6, p, blockquote, pre,
@@ -84,8 +86,16 @@ const GlobalStyles = createGlobalStyle`
     font-style: normal;
   }
 
-  * {
+  body, * {
     font-family: 'DNFBitBitTTF';
+  }
+
+  body {
+    display: flex;
+    min-height: 100vh;
+    justify-content: center;
+    background-image: url(${background});
+    background-size: 100% 100%;
   }
 `;
 
