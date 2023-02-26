ff3e5c2952c730c5a3bd30c1b02c79b85cc30672
sodalite1204 <your.jiny.9@gmail.com>
2023-02-26 22:51:13 +0900

    [feat] Add diary style

--- src/components/DiaryListPage/DiaryListPage.style.ts
+++ src/components/DiaryListPage/DiaryListPage.style.ts
@@ -1,27 +1,68 @@
 import styled from 'styled-components';
 
 export const Container = styled.section`
+  width: 400px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   margin-top: 50px;
+
+  @media screen and (max-width: 400px) {
+    width: 340px;
+    min-height: 98px;
+  }
 `;
 
 export const DateText = styled.h2`
-  margin-left: 8px;
+  margin: 0 1rem;
+  font-size: 1.5rem;
+  font-family: 'DNFBitBitTTF';
+  color: ${({ theme }) => theme.color.primaryDark};
 `;
+
 export const DiaryListHeader = styled.header`
   display: flex;
+  align-items: center;
+  width: 100%;
+  margin-bottom: 2rem;
+`;
+
+export const MonthWrapper = styled.div`
+  flex: 9;
+  display: flex;
+  margin-left: 2rem;
   justify-content: center;
+  align-items: center;
+
+  svg {
+    width: 10px;
+  }
+`;
+
+export const SortButtonWrapper = styled.div`
+  flex: 1;
+
+  svg {
+    width: 24px;
+  }
 `;
 
 export const ChangeMonthButton = styled.button`
   margin-left: 8px;
 `;
 
-export const SortButton = styled.select`
-  margin-left: 8px;
+export const UpButtonWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
 `;
 
-export const ScrollToTopButton = styled.button``;
+export const ScrollToTopButton = styled.button`
+  min-height: 1.25rem;
+  padding: 0.4rem 1rem;
+  border-radius: 28px;
+  margin-top: 0.3rem;
+  color: ${({ theme }) => theme.color.white};
+  background-color: ${({ theme }) => theme.color.primaryDark};
+`;
