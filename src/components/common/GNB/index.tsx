9e520196d78fe2cde2b6aba25aa2bcd8afacc780
Dae-une <ko301201@naver.com>
2023-02-27 16:17:31 +0900

    feat: GNB 공유 버튼시 준비중 모달 생성

--- src/components/common/GNB/index.tsx
+++ src/components/common/GNB/index.tsx
@@ -1,18 +1,18 @@
+import { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 import * as S from './GNB.styles';
+import Modal from '../Modal';
 
 import GNB_SVG from '@/assets/GNB';
-const LEFT_LINKS = [
-  { title: '공유', link: '' },
-  { title: '추억', link: '/diaries' },
-];
+
 const RIGHT_LINKS = [
   { title: '달력', link: '/calendar' },
   { title: '설정', link: '/setting' },
 ];
 
 const GNB = () => {
+  const [showModal, setShowModal] = useState(false);
   const navigate = useNavigate();
   const { pathname } = useLocation();
 
@@ -20,39 +20,43 @@ const GNB = () => {
     return pathname === link;
   };
 
+  const handleModal = () => {
+    setShowModal((prev) => !prev);
+  };
+
   if (pathname === '/write') {
     return null;
   }
 
+  console.log(showModal);
+
   return (
-    <S.GNBContainer>
-      {GNB_SVG.container}
-      <S.NavIcons>
-        <S.SideIcons>
-          {LEFT_LINKS.map((link) => (
-            <S.GNBButton
-              key={link.title}
-              onClick={() => navigate(link.link)}
-              isSelected={isSelected(link.link)}
-            >
-              {link.title}
+    <>
+      <S.GNBContainer>
+        {GNB_SVG.container}
+        <S.NavIcons>
+          <S.SideIcons>
+            <S.GNBButton onClick={handleModal}>공유</S.GNBButton>
+            <S.GNBButton onClick={() => navigate('/diaries')} isSelected={isSelected('/diaries')}>
+              추억
             </S.GNBButton>
-          ))}
-        </S.SideIcons>
-        <S.HomeButton onClick={() => navigate('/')}>{GNB_SVG.homeButton}</S.HomeButton>
-        <S.SideIcons>
-          {RIGHT_LINKS.map((link) => (
-            <S.GNBButton
-              key={link.title}
-              onClick={() => navigate(link.link)}
-              isSelected={isSelected(link.link)}
-            >
-              {link.title}
-            </S.GNBButton>
-          ))}
-        </S.SideIcons>
-      </S.NavIcons>
-    </S.GNBContainer>
+          </S.SideIcons>
+          <S.HomeButton onClick={() => navigate('/')}>{GNB_SVG.homeButton}</S.HomeButton>
+          <S.SideIcons>
+            {RIGHT_LINKS.map((link) => (
+              <S.GNBButton
+                key={link.title}
+                onClick={() => navigate(link.link)}
+                isSelected={isSelected(link.link)}
+              >
+                {link.title}
+              </S.GNBButton>
+            ))}
+          </S.SideIcons>
+        </S.NavIcons>
+      </S.GNBContainer>
+      {showModal && <Modal onClose={handleModal} />}
+    </>
   );
 };
 
