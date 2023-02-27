8341b7fb2f8072cdf7765170db4fafdca8d1fd28
SolbiShin <sbshin.dev@gmail.com>
2023-02-27 15:41:16 +0900

    불필요한 이미지 파일 제거

--- src/components/common/Modal/index.tsx
+++ src/components/common/Modal/index.tsx
@@ -1,6 +1,7 @@
 import * as S from './Modal.style';
 
 import { ReactComponent as CloseButton } from '@/assets/commonIcon/close.svg';
+import SadActive from '@/assets/emotionIcon/sad_active.svg';
 
 interface Props {
   onClose: () => void;
@@ -13,14 +14,14 @@ interface Props {
 const Modal = ({
   onClose,
   text = '아직 준비중입니다....',
-  imagePath = '/우는얼굴.svg',
+  imagePath,
   buttonText,
   handleButtonEvent,
 }: Props) => {
   return (
     <S.Dimmer>
       <S.ModalContainer>
-        <img src={imagePath} width="180rem" height="150rem" alt="sad" />
+        <img src={imagePath || SadActive} width="180rem" height="150rem" alt="sad" />
         <S.ModalCloseButton onClick={onClose} aria-label="close">
           <CloseButton width="2rem" height="2rem" />
         </S.ModalCloseButton>
