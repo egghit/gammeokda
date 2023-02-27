000db27916a92b4c5e61256fe34d6bd1a9bb3cc1
se <63814960+se030@users.noreply.github.com>
2023-02-27 14:35:18 +0900

    Feat/post-diary: 일기 작성 페이지 기능 구현 (#25)
    * [feat] DiaryForm 기능 구현
    
    임시로 POST 요청 대신 콘솔에 값 보여주고 뒤로가기 처리
    
    Co-authored-by: 바카로이 <parkharoi@users.noreply.github.com>
    
    * [feat] DiaryWritePage 기능 추가 및 리팩토링
    
    - 선택한 감정에 따라 다마고치 이미지 변경
    - 뒤로 가기 버튼 추가
    
    ---------
    
    Co-authored-by: 바카로이 <parkharoi@users.noreply.github.com>

---
+++ src/components/DiaryWritePage/GobackButton/GobackButton.style.ts
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  padding-top: 1rem;
+`;
+
+export const Button = styled.button`
+  position: absolute;
+  top: 2rem;
+  left: 1rem;
+`;
