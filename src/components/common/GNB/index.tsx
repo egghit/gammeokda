6ab08230b99f7e0d22b1c9003a542aa53022dd19
Dae-une <ko301201@naver.com>
2023-02-27 15:17:21 +0900

    feat: 페이지에 맞게 GNB 버튼 색상 변경

--- src/components/common/GNB/index.tsx
+++ src/components/common/GNB/index.tsx
@@ -1,38 +1,50 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import * as Style from './GNB.styles';
 
 import GNB_SVG from '@/assets/GNB';
-// 달력 , 추억, 공유, 설정
 const LEFT_LINKS = [
-  { title: '공유', link: '/' },
+  { title: '공유', link: '' },
   { title: '추억', link: '/diaries' },
 ];
 const RIGHT_LINKS = [
   { title: '달력', link: '/calendar' },
-  { title: '설정', link: '/' },
+  { title: '설정', link: '/setting' },
 ];
 
 const GNB = () => {
   const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  const isSelected = (link: string) => {
+    return pathname === link;
+  };
+
   return (
     <Style.GNBContainer>
       {GNB_SVG.container}
       <Style.NavIcons>
         <Style.SideIcons>
           {LEFT_LINKS.map((link) => (
-            <button key={link.title} onClick={() => navigate(link.link)}>
+            <Style.GNBButton
+              key={link.title}
+              onClick={() => navigate(link.link)}
+              isSelected={isSelected(link.link)}
+            >
               {link.title}
-            </button>
+            </Style.GNBButton>
           ))}
         </Style.SideIcons>
         <Style.HomeButton onClick={() => navigate('/')}>{GNB_SVG.homeButton}</Style.HomeButton>
         <Style.SideIcons>
           {RIGHT_LINKS.map((link) => (
-            <button key={link.title} onClick={() => navigate(link.link)}>
+            <Style.GNBButton
+              key={link.title}
+              onClick={() => navigate(link.link)}
+              isSelected={isSelected(link.link)}
+            >
               {link.title}
-            </button>
+            </Style.GNBButton>
           ))}
         </Style.SideIcons>
       </Style.NavIcons>
