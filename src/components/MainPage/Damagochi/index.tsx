6889a806627880b4e801757d617380e9f4bfe5c2
Dae-une <ko301201@naver.com>
2023-02-27 18:15:01 +0900

    feat: IOS 에서 쓰다듬기 미지원 모달

--- src/components/MainPage/Damagochi/index.tsx
+++ src/components/MainPage/Damagochi/index.tsx
@@ -1,4 +1,4 @@
-import { Dispatch, SetStateAction, useEffect, useReducer } from 'react';
+import { Dispatch, SetStateAction, useEffect, useReducer, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 import * as S from './damagochi.styels';
@@ -12,6 +12,7 @@ import {
 
 import { DamagochiAnimation } from '@/assets/damagochi';
 import DamagochiTheme from '@/assets/damagochiCase';
+import Modal from '@/components/common/Modal';
 
 interface DamagochiProps {
   name: string;
@@ -26,12 +27,17 @@ const Damagochi = ({ name, age, isGrow, setIsGrow }: DamagochiProps) => {
   const { state } = location;
   const [caseTheme, caseDispatch] = useReducer(caseReducer, caseInitialState);
   const [damagochiState, damagochiDispatch] = useReducer(damagochiReducer, damagochiInitailState);
+  const [showModal, setShowModal] = useState(false);
 
   const damagochiAnimation = DamagochiAnimation[`${age}`][`${damagochiState.animation}`];
-  // const damagochiAnimation = DamagochiAnimation.egg.default;
+
+  const isIOSSafari = navigator.userAgent.match(/like Mac OS X/i) ? true : false;
 
   const handleTheme = () => {
     caseDispatch('click');
+  };
+  const handelModal = () => {
+    setShowModal((prev) => !prev);
   };
 
   const handelAnimation = (action: DamagochiAction) => {
@@ -55,18 +61,23 @@ const Damagochi = ({ name, age, isGrow, setIsGrow }: DamagochiProps) => {
   }, [state, isGrow]);
 
   return (
-    <S.Layout>
-      <S.Container>
-        {DamagochiTheme[caseTheme]}
-        <S.CharName>
-          <h1>{name}</h1>
-        </S.CharName>
-        <S.Character>{damagochiAnimation}</S.Character>
-        <S.LeftButton onClick={handleTheme}>테마변경</S.LeftButton>
-        <S.CenterButton onClick={() => navigate('/write')}>감정주기</S.CenterButton>
-        <S.RightButton onClick={() => handelAnimation('stroke')}>쓰다듬기</S.RightButton>
-      </S.Container>
-    </S.Layout>
+    <>
+      <S.Layout>
+        <S.Container>
+          {DamagochiTheme[caseTheme]}
+          <S.CharName>
+            <h1>{name}</h1>
+          </S.CharName>
+          <S.Character>{damagochiAnimation}</S.Character>
+          <S.LeftButton onClick={handleTheme}>테마변경</S.LeftButton>
+          <S.CenterButton onClick={() => navigate('/write')}>감정주기</S.CenterButton>
+          <S.RightButton onClick={isIOSSafari ? handelModal : () => handelAnimation('stroke')}>
+            쓰다듬기
+          </S.RightButton>
+        </S.Container>
+      </S.Layout>
+      {showModal && <Modal onClose={handelModal} text="IOS환경에서는 지원되지 않습니다." />}
+    </>
   );
 };
 
