7c328c6eae7e1c4b8632ac45186ff2906c8368c0
se <63814960+se030@users.noreply.github.com>
2023-02-27 18:59:59 +0900

    Feat/polish-css: CSS 최종 수정 작업 (#35)
    * feat: 설정되지 않은 폰트 컬러 추가
    
    * [feat] meta tag maximum-scale=1 추가
    
    * [feat] 쓰기 페이지 textarea font-size 추가
    
    * [feat] 데스크탑 레이아웃 추가
    
    * [feat] 데스크탑 페이지 구성

---
+++ src/pages/Layout.tsx
@@ -0,0 +1,33 @@
+import { ReactNode } from 'react';
+import styled from 'styled-components';
+
+import GNB from '@/components/common/GNB';
+import DesktopPage from '@/pages/DesktopPage';
+
+interface LayoutProps {
+  router: ReactNode;
+}
+
+const Layout = ({ router }: LayoutProps) => {
+  return (
+    <MainContainer>
+      <DesktopPage />
+      <PageContainer>
+        {router}
+        <GNB />
+      </PageContainer>
+    </MainContainer>
+  );
+};
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 2rem;
+`;
+
+const PageContainer = styled.div`
+  position: relative;
+`;
+
+export default Layout;
--- src/pages/index.style.ts
+++ src/pages/index.style.ts
@@ -1,15 +1,21 @@
 import styled from 'styled-components';
 
 export const Container = styled.section`
-  width: 100%;
+  width: 400px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   margin: 2rem auto 12rem auto;
 
-  @media screen and (max-width: 400px) {
-    width: 340px;
+  @media screen and (max-width: 768px) {
+    width: 480px;
+  }
+
+  @media screen and (max-width: 480px) {
+    width: 100vw;
+    max-width: 100vw;
     min-height: 98px;
+    padding: 0 0.5rem;
   }
 `;
