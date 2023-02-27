5b72601d94a219588728156615cd353823b98b93
se030 <se030.kim@gmail.com>
2023-02-27 13:20:11 +0900

    feat: NamingForm 기능 임시 구현

--- src/components/SignupPage/NamingForm/index.tsx
+++ src/components/SignupPage/NamingForm/index.tsx
@@ -7,13 +7,18 @@ import { ReactComponent as DamagochiEgg } from '@/assets/damagochi_egg.svg';
 import Button from '@/components/common/Button';
 
 const NamingForm = () => {
-  const [name, setName] = useState('곶감이');
   const navigate = useNavigate();
 
-  const onSubmit: FormEventHandler = (e) => {
+  const onSubmit: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
 
-    localStorage.setItem('name', name);
+    const target = e.target as HTMLFormElement;
+    if (!target) return;
+
+    const data = new FormData(target);
+    const name = data.get('name');
+
+    localStorage.setItem('name', name?.toString() ?? DEFAULT_NAME);
     navigate('/diaries');
   };
 
@@ -24,7 +29,7 @@ const NamingForm = () => {
         <br />
         이름을 지어주세요!
       </S.Title>
-      <S.Input defaultValue="곶감이" />
+      <S.Input name="name" placeholder={DEFAULT_NAME} />
       <DamagochiEgg width={300} height={400} role="img" aria-label="다마고치 알" />
       <S.Alert>
         한번 지은 이름은 변경이 불가하니 <br />
@@ -34,5 +39,7 @@ const NamingForm = () => {
     </S.Form>
   );
 };
+
+const DEFAULT_NAME = '곶감이';
 
 export default NamingForm;
