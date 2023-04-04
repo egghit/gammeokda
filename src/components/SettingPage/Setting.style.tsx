f64ef42a3b0a1dc7312ae7062c4618872c41f7d0
se030 <se030.kim@gmail.com>
2023-04-04 14:01:05 +0900

    refactor: style props 구조 분해 할당

--- src/components/SettingPage/Setting.style.tsx
+++ src/components/SettingPage/Setting.style.tsx
@@ -10,12 +10,12 @@ export const IntroContainer = styled.div`
 `;
 
 export const ServiceTitle = styled.div`
-  color: ${(props) => props.theme.color.primaryDark};
+  color: ${({ theme }) => theme.color.primaryDark};
   font-size: 2rem;
   margin-bottom: 0.25rem;
 
   & > p {
-    color: ${(props) => props.theme.color.primary};
+    color: ${({ theme }) => theme.color.primary};
     font-size: 1.5rem;
   }
 `;
@@ -30,14 +30,14 @@ export const MenuWrapper = styled.section`
 
 export const SettingTitle = styled.h2`
   margin-left: 0.5rem;
-  color: ${(props) => props.theme.color.black};
+  color: ${({ theme }) => theme.color.black};
   font-size: 1.5rem;
 `;
 
 export const SettingMenuLink = styled.a`
   width: max-content;
   height: fit-content;
-  color: ${(props) => props.theme.color.titlePink};
+  color: ${({ theme }) => theme.color.titlePink};
   display: block;
   font-size: 1.25rem;
 `;
@@ -45,7 +45,7 @@ export const SettingMenuLink = styled.a`
 export const SettingMenuButton = styled.button`
   width: max-content;
   height: fit-content;
-  color: ${(props) => props.theme.color.titlePink};
+  color: ${({ theme }) => theme.color.titlePink};
   font-size: 1.25rem;
 `;
 
@@ -56,7 +56,7 @@ export const MenuItemWrapper = styled.div`
   width: 100%;
   border-radius: 20px;
   padding: 20px 310px 19px 26px;
-  background-color: ${(props) => props.theme.color.white};
+  background-color: ${({ theme }) => theme.color.white};
 
   button:first-child,
   a:first-child {
